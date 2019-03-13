b9b58f5e6948bfc7777b57ef68dc0c79c2ee643e
Dawlin Pena Luciano <dawmusic19@gmail.com>
2019-03-13 19:43:38 +0000

    Already visited point not counted as cleaned, test green

--- RobotCleaner.Tests/RobotCommandsUnitTest.cs
+++ RobotCleaner.Tests/RobotCommandsUnitTest.cs
@@ -42,6 +42,19 @@ namespace RobotCleaner.Tests
             roomsCleaned.ShouldBe(3);
         }
 
+        [Fact]
+        public void When_moving_2_step_to_N__1_to_S_and_1_to_E_then_three_rooms_should_be_cleaned()
+        {
+            var robot = new Robot();
+            var commands = new Queue<CommandRequest>();
+            commands.Enqueue(new CommandRequest(Direction.N, 2));
+            commands.Enqueue(new CommandRequest(Direction.E, 1));
+            var startingPoint = new Point(0, 0);
+
+            var roomsCleaned = robot.CleanRooms(startingPoint, commands);
+            roomsCleaned.ShouldBe(3);
+        }
+
         public static TheoryData<CommandRequest> MovingOneStepAnyDirectionData =>
             new TheoryData<CommandRequest>
             {
