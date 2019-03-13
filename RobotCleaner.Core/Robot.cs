b16c47639e722911a68b82b8e7499112232b1788
Dawlin Pena Luciano <dawmusic19@gmail.com>
2019-03-13 19:40:19 +0000

    Algorith and one or two moves tests passed

--- RobotCleaner.Core/Robot.cs
+++ RobotCleaner.Core/Robot.cs
@@ -6,18 +6,44 @@ namespace RobotCleaner.Core
 {
     public class Robot
     {
-        public int CleanRooms(Point startingPoint)
-        {
-            var cleanedPoints = new HashSet<Point>()
-            {
-                startingPoint
-            };
-            return cleanedPoints.Count;
-        }
+        public int CleanRooms(Point startingPoint) =>
+            CleanRooms(startingPoint, new Queue<CommandRequest>());
 
         public int CleanRooms(Point startingPoint, Queue<CommandRequest> commands)
         {
-            throw new NotImplementedException();
+            var cleanedPoints = new HashSet<Point>
+            {
+                startingPoint
+            };
+            CommandRequest command;
+            var currentPoint = startingPoint;
+            while (commands.TryDequeue(out command))
+            {
+                currentPoint = Move(command, currentPoint);
+                cleanedPoints.Add(currentPoint);
+            }
+            return cleanedPoints.Count;
+        }
+
+        private Point Move(CommandRequest command, Point currentPoint)
+        {
+            switch (command.Direction)
+            {
+                case Direction.E:
+                    currentPoint.X--;
+                    break;
+                case Direction.W:
+                    currentPoint.X++;
+                    break;
+                case Direction.N:
+                    currentPoint.Y++;
+                    break;
+                case Direction.S:
+                    currentPoint.Y--;
+                    break;
+            }
+
+            return new Point(currentPoint.X, currentPoint.Y);
         }
     }
 }
--- RobotCleaner.Tests/RobotCommandsUnitTest.cs
+++ RobotCleaner.Tests/RobotCommandsUnitTest.cs
@@ -17,15 +17,38 @@ namespace RobotCleaner.Tests
             roomsCleaned.ShouldBe(1);
         }
 
-        [Fact]
-        public void When_execute_command_1_step_to_any_direction_then_two_rooms_should_be_cleaned()
+        [Theory]
+        [MemberData(nameof(MovingOneStepAnyDirectionData))]
+        public void When_moving_1_step_to_any_direction_then_two_rooms_should_be_cleaned(CommandRequest command)
         {
             var robot = new Robot();
             var commands = new Queue<CommandRequest>();
-            commands.Enqueue(new CommandRequest(Direction.N, 1));
+            commands.Enqueue(command);
             var startingPoint = new Point(0, 0);
             var roomsCleaned = robot.CleanRooms(startingPoint, commands);
             roomsCleaned.ShouldBe(2);
         }
+
+        [Fact]
+        public void When_moving_1_step_to_N_and_1_to_E_then_three_rooms_should_be_cleaned()
+        {
+            var robot = new Robot();
+            var commands = new Queue<CommandRequest>();
+            commands.Enqueue(new CommandRequest(Direction.N, 1));
+            commands.Enqueue(new CommandRequest(Direction.E, 1));
+            var startingPoint = new Point(0, 0);
+
+            var roomsCleaned = robot.CleanRooms(startingPoint, commands);
+            roomsCleaned.ShouldBe(3);
+        }
+
+        public static TheoryData<CommandRequest> MovingOneStepAnyDirectionData =>
+            new TheoryData<CommandRequest>
+            {
+                new CommandRequest(Direction.N, 1),
+                new CommandRequest(Direction.S, 1),
+                new CommandRequest(Direction.E, 1),
+                new CommandRequest(Direction.W, 1)
+            };
     }
 }
